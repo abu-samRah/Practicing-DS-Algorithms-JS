56da19072f6448569097dd87487b21d0d558e265
abuSamrah <abdullah@kiitos-tech.com>
2022-01-16 08:15:32 +0200

    cleaned main index.js folder

--- components/DS/LinkedLists/index.js
+++ components/DS/LinkedLists/index.js
@@ -117,3 +117,32 @@ class LinkedList {
 
 
 export default LinkedList
+
+
+
+/**
+ Add this to the main index.js file to start using and testing linkedList
+
+ import LinkedList from './components/DS/LinkedLists/index.js' 
+
+const linkedList = new LinkedList(1)
+
+linkedList.insert(5)
+
+linkedList.insertAtBeginning(0)
+linkedList.insertAfter(1,12)
+linkedList.remove(0)
+linkedList.pop()
+
+
+linkedList.insert(80)
+linkedList.insert(90)
+
+linkedList.insert(45)
+linkedList.insert(34)
+
+linkedList.insert(23)
+linkedList.insert(68)
+linkedList.sort()
+linkedList.print()
+ */
